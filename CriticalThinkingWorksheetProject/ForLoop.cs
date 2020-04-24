c384431d7e07e634dcac587c905fb23ca9776fda
Steven Morimoto <smorimoto2284@gmail.com>
2020-04-24 08:52:20 -0500

    Finished the forloop step 2

---
+++ CriticalThinkingWorksheetProject/ForLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingWorksheetProject
+{
+    class ForLoop
+    {
+        //variables
+        int i;
+
+        //constructor
+        public ForLoop()
+        {
+            i = 0;
+        }   
+
+        //methods
+        public void RunLoop()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
--- CriticalThinkingWorksheetProject/Program.cs
+++ CriticalThinkingWorksheetProject/Program.cs
@@ -10,6 +10,8 @@ namespace CriticalThinkingWorksheetProject
     {
         static void Main(string[] args)
         {
+            ForLoop loops = new ForLoop();
+            loops.RunLoop();
         }
     }
 }
