545af656f00c15be5683f91bd9652906d55d0750
Steven Morimoto <smorimoto2284@gmail.com>
2020-05-04 11:00:12 -0500

    completed calculatormadness part of project/assignment.  moving onto 'Fun with Classes' section.

--- CriticalThinkingWorksheetProject/CalculatorMadness.cs
+++ CriticalThinkingWorksheetProject/CalculatorMadness.cs
@@ -11,39 +11,44 @@ namespace CriticalThinkingWorksheetProject
         public int AddTwoNumbers(int numberOne, int numberTwo)
         {
             int result = numberOne + numberTwo;
-            Console.WriteLine("The result of this additions is: " + result);
+            //Console.WriteLine("The result of this additions is: " + result);
             return result;
         }
         public int SubtractNumbers(int numberOne, int numberTwo)
         {
             int result = numberOne - numberTwo;
-            Console.WriteLine("The result of this subtractions is: " + result);
+            //Console.WriteLine("The result of this subtractions is: " + result);
             return result;
         }
         public int MultiplyNumbers(int numberOne, int numberTwo)
         {
             int result = numberOne * numberTwo;
-            Console.WriteLine("The result of this multiplication is: " + result);
+            //Console.WriteLine("The result of this multiplication is: " + result);
             return result;
         }
         public int DivideNumbers(int numberOne, int numberTwo)
         {
             int result = numberOne / numberTwo;
-            Console.WriteLine("The result of the division is: " + result);
+            //Console.WriteLine("The result of the division is: " + result);
             return result;
         }
 
 
         public void RunCalculations()
         {
-            AddTwoNumbers(48, 250);
-            Console.WriteLine("Please enter the first number to add:");
-            int valueOne = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the second number to add:");
-            int valueTwo = int.Parse(Console.ReadLine());
-            int output = AddTwoNumbers(valueOne, valueTwo);
-            
-            Console.WriteLine("The result is: " + output);
+            //6+5-40*35/4+2*2
+            int lineOne = SubtractNumbers(11, DivideNumbers(MultiplyNumbers(40, 35), 4));
+
+
+            int lineTwo = MultiplyNumbers(2, 2);
+
+            //Console.WriteLine("Please enter problem to solve :");
+            //int mathProblem = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the second number to add:");
+            //int valueTwo = int.Parse(Console.ReadLine());
+            int output = AddTwoNumbers(lineOne, lineTwo);
+
+            Console.WriteLine(output);
             Console.ReadLine();
 
         }
